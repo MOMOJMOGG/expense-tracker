0272d60ccd811d5e7a5673ce63284be52c3cc8db
MOMOJMOGG <momomacaca@gmail.com>
2021-06-05 18:20:53 +0800

    feat: add passport config and user model

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const methodOverride = require('method-override')
 const session = require('express-session')
 const moment = require('moment')
 const routes = require('./routes')
+const usePassport = require('./config/passport') // 載入設定檔，要寫在 express-session 以後
+const flash = require('connect-flash')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const app = express()
 const PORT = process.env.PORT
@@ -52,6 +58,19 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }))
+
+usePassport(app)
+
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
+  res.locals.errors = req.flash('errors')
+  next()
+})
 
 app.use(routes)
 
---
+++ config/passport.js
@@ -0,0 +1,70 @@
+const bcrypt = require('bcryptjs')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
+const User = require('../models/user')
+
+module.exports = app => {
+  // 初始化 Passport 模組
+  app.use(passport.initialize())
+  app.use(passport.session())
+
+  // passport 本地登入策略
+  passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, async (req, email, password, done) => {
+    try {
+      const user = await User.findOne({ email })
+      if (!user) {
+        return done(null, false, req.flash('errors', { message: 'That email is not registered!' }))
+      }
+      const isMatch = await bcrypt.compare(password, user.password)
+      if (!isMatch) {
+        return done(null, false, req.flash('errors', { message: 'Email or Password incorrect.' }))
+      }
+
+      return done(null, user)
+    } catch (err) {
+      return done(err, false)
+    }
+  }))
+
+  // passport Facebook 登入策略
+  passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: process.env.FACEBOOK_APP_CALLBACK_URL,
+    profileFields: ['email', 'displayName']
+  }, async (accessToken, refreshToken, profile, done) => {
+    try {
+      const { name, email } = profile._json
+      let user = User.findOne({ email })
+      if (user) {
+        return done(null, user)
+      }
+
+      const randomPassword = Math.random().toString(36).slice(-8) // 隨機產生 0~1小數 | 36: 26英文字母 + 10 數字總數 | 擷取最後8碼
+      const salt = bcrypt.genSalt(10)
+      const hash = bcrypt.hash(randomPassword, salt)
+      user = await User.create({ name, email, password: hash })
+      if (user) {
+        return done(null, user)
+      }
+    } catch (err) {
+      return done(err, false)
+    }
+  }))
+
+  // 設定序列化與反序列化
+  passport.serializeUser((user, done) => {
+    done(null, user.id)
+  })
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findById(id).lean()
+      if (user) {
+        return done(null, user)
+      }
+    } catch (err) {
+      return done(err, null)
+    }
+  })
+}
